4294d327602884c37233b73ac0a5ecb6f8ec07ed
Sam <armandawoodkhan@gmail.com>
2025-10-03 23:18:31 +0530

    fixedMarque

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -7,19 +7,18 @@ function Banner() {
     "Social Media Marketing",
     "Content Marketing",
   ];
+
   return (
-    <div className='w-screen relative left-1/2 -translate-x-1/2 bg-[#7ED957] text-[#10012B] overflow-x-hidden py-4 whitespace-nowrap'>
-      <div className='flex animate-marquee'>
-        {services.map((service, ind) => (
-          <div key={ind} className="flex flex-shrink-0 gap-2 items-center text-5xl mx-4">
-            <div>{service}</div>
-            <Image src={'./bannerStar.svg'} alt="" width={56} height={56} />
-          </div>
-        ))}
-        {services.map((service, ind) => (
-          <div key={`dup-${ind}`} aria-hidden="true" className="flex flex-shrink-0 gap-2 items-center text-5xl mx-4">
-            <div>{service}</div>
-            <Image src={'./bannerStar.svg'} alt="" width={56} height={56} />
+    <div className="w-screen relative left-1/2 -translate-x-1/2 bg-[#7ED957] text-[#10012B] overflow-hidden py-4">
+      <div className="flex w-max animate-marquee">
+        {/* Repeat services twice for seamless loop */}
+        {[...services, ...services].map((service, ind) => (
+          <div
+            key={ind}
+            className="flex flex-shrink-0 gap-2 items-center text-5xl mx-4"
+          >
+            <span>{service}</span>
+            <Image src="/bannerStar.svg" alt="star" width={56} height={56} />
           </div>
         ))}
       </div>
